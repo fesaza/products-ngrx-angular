7a896892f35cd7aefa00e88fdfc972a3ce540ae6
fesaza <fesaza@gmail.com>
2018-05-15 20:03:23 -0500

    initial config - material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { SharedComponentsModule } from 'src/app/components/shared-components.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
